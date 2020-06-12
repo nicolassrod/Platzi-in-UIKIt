fbf9b0649575d4890820ce72703edf32d861c3b0
Nicolás A. Rodríguez <nrq5510@gmail.com>
2020-06-11 20:29:38 -0500

    Add PostCell to ExploreViewController Add PostView

---
+++ Platzi in UIKIt/Components/PostView.swift
@@ -0,0 +1,139 @@
+//
+//  PostView.swift
+//  Platzi in UIKIt
+//
+//  Created by Nicolás A. Rodríguez on 6/10/20.
+//  Copyright © 2020 Nicolás A. Rodríguez. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class PostView: UIView {
+    private lazy var stack1: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .fill
+        stack.spacing = 12
+        return stack
+    }()
+
+    private lazy var stack2: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 12
+        return stack
+    }()
+    
+    private lazy var darckEfect: UIView = {
+        let darckEfect = UIView()
+        darckEfect.backgroundColor = .black
+        darckEfect.alpha = 0.5
+        return darckEfect
+    }()
+    
+    lazy var postBackgroundImage: UIImageView = {
+        let postBackgroundImage = UIImageView()
+        postBackgroundImage.clipsToBounds = true
+        postBackgroundImage.image = UIImage(named: "adwords-background")
+        postBackgroundImage.contentMode = .scaleAspectFill
+        return postBackgroundImage
+    }()
+
+    lazy var postUserImage: UIImageView = {
+        let postUserImage = UIImageView()
+        postUserImage.clipsToBounds = true
+        postUserImage.image = UIImage(named: "avatar")
+        postUserImage.contentMode = .scaleAspectFill
+        postUserImage.layer.cornerRadius = 20
+        return postUserImage
+    }()
+
+
+    lazy var postUsername: UILabel = {
+        let postUsername = UILabel()
+        postUsername.text = "isisconvelo"
+        postUsername.numberOfLines = 1
+        postUsername.font = .systemFont(ofSize: 16, weight: .bold)
+        postUsername.textColor = .white
+        return postUsername
+    }()
+
+    lazy var postTitle: UILabel = {
+        let postTitle = UILabel()
+        postTitle.text = "Habla 3 idiomas y dirige la Escuela de Inglés de Platzi, ¿sabes quién es"
+        postTitle.numberOfLines = 0
+        postTitle.font = .boldSystemFont(ofSize: 18)
+        postTitle.textColor = .white
+        return postTitle
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
+        addSubview(postBackgroundImage)
+        addSubview(darckEfect)
+        addSubview(stack1)
+        addSubview(stack2)
+        stack1.addArrangedSubview(postTitle)
+        stack1.addArrangedSubview(stack2)
+        stack2.addArrangedSubview(postUserImage)
+        stack2.addArrangedSubview(postUsername)
+//        addSubview(postUserImage)
+//        addSubview(postUsername)
+//        addSubview(postTitle)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        postBackgroundImage.translatesAutoresizingMaskIntoConstraints = false
+        postBackgroundImage.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        postBackgroundImage.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        postBackgroundImage.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        postBackgroundImage.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+
+        darckEfect.translatesAutoresizingMaskIntoConstraints = false
+        darckEfect.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        darckEfect.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        darckEfect.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        darckEfect.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+        stack1.translatesAutoresizingMaskIntoConstraints = false
+        stack1.topAnchor.constraint(greaterThanOrEqualTo: self.topAnchor, constant: 20).isActive = true
+        stack1.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 20).isActive = true
+        stack1.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -20).isActive = true
+        stack1.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20).isActive = true
+        
+        postUserImage.translatesAutoresizingMaskIntoConstraints = false
+        postUserImage.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
+        postUserImage.widthAnchor.constraint(equalToConstant: 40.0).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+struct PostView_Previews: PreviewProvider {
+    static var previews: some View {
+        Container()
+            .previewLayout(.fixed(width: 250, height: 220))
+    }
+    
+    struct Container: UIViewRepresentable {
+        func makeUIView(context: Context) -> UIView {
+            let view = PostView()
+            return view
+        }
+        
+        func updateUIView(_ uiView: UIView, context: Context) {
+            
+        }
+        
+        typealias UIViewType = UIView
+    }
+}
--- Platzi in UIKIt/ExploreController.swift
+++ Platzi in UIKIt/ExploreController.swift
@@ -68,7 +68,7 @@ class ExploreController: UICollectionViewController {
                 item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
                 
                 let group = NSCollectionLayoutGroup.vertical(layoutSize:
-                    .init(widthDimension: .fractionalWidth(0.70), heightDimension: .estimated(165)), subitem: item, count: 1)
+                    .init(widthDimension: .fractionalWidth(0.70), heightDimension: .estimated(210)), subitem: item, count: 1)
                 group.interItemSpacing = .fixed(8)
                 
                 let section = NSCollectionLayoutSection(group: group)
@@ -144,6 +144,9 @@ class ExploreController: UICollectionViewController {
         } else if indexPath.section == 2 {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pathCellId", for: indexPath) as! PathCell
             return cell
+        } else if indexPath.section == 3 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "postCellId", for: indexPath) as! PostCell
+            return cell
         } else {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "defaultCellId", for: indexPath)
             cell.backgroundColor = .red
@@ -161,6 +164,7 @@ class ExploreController: UICollectionViewController {
         collectionView.register(FeaturedCourseCell.self, forCellWithReuseIdentifier: "featuredCourseCellId")
         collectionView.register(CourseCell.self, forCellWithReuseIdentifier: "courseCellId")
         collectionView.register(PathCell.self, forCellWithReuseIdentifier: "pathCellId")
+        collectionView.register(PostCell.self, forCellWithReuseIdentifier: "postCellId")
     }
     
     required init?(coder: NSCoder) {
@@ -211,7 +215,7 @@ class FeaturedCourseCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(course)
+        contentView.addSubview(course)
     }
     
     override func layoutSubviews() {
@@ -232,7 +236,7 @@ class CourseCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(course)
+        contentView.addSubview(course)
     }
     
     override func layoutSubviews() {
@@ -253,7 +257,7 @@ class PathCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(path)
+        contentView.addSubview(path)
     }
     
     override func layoutSubviews() {
@@ -265,6 +269,27 @@ class PathCell: UICollectionViewCell {
     }
     
     required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class PostCell: UICollectionViewCell {
+    var post = PostView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(post)
+    }
+    
+    override func layoutSubviews() {
+        post.translatesAutoresizingMaskIntoConstraints = false
+        post.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        post.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        post.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        post.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
